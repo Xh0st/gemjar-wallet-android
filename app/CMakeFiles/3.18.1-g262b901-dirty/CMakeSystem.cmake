9fd8b84b6365f33b425da154be27968b8ae1f0c6
Xh0st <and.andrei@outlook.com>
2022-10-09 17:26:55 +0300

    Update CMakeSystem.cmake

--- app/CMakeFiles/3.18.1-g262b901-dirty/CMakeSystem.cmake
+++ app/CMakeFiles/3.18.1-g262b901-dirty/CMakeSystem.cmake
@@ -3,7 +3,7 @@ set(CMAKE_HOST_SYSTEM_NAME "Windows")
 set(CMAKE_HOST_SYSTEM_VERSION "10.0.19044")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "AMD64")
 
-include("E:/dev/Android/ndk/21.4.7075529/build/cmake/android.toolchain.cmake")
+include("C:/Users/andan/AppData/Local/Android/Sdk/ndk/21.4.7075529/build/cmake/android.toolchain.cmake")
 
 set(CMAKE_SYSTEM "Android-1")
 set(CMAKE_SYSTEM_NAME "Android")
